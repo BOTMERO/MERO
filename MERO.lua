191c5f578a97f92ad4e8e26d57a761cc26e897fa
BOTMERO <63414617+BOTMERO@users.noreply.github.com>
2020-04-09 20:04:42 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -3146,7 +3146,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if SudoBot(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/MEROTEAM/Files_MERO/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/TEAMMERO/Files_Boyka/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3184,7 +3184,7 @@ t = "📁| الملف ← "..file.."\n🔰| تم تعطيل ملف \n"
 else
 t = "🔖| بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MEROTEAM/Files_MERO/master/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/TEAMMERO/Files_Boyka/master/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3204,7 +3204,7 @@ t = "🔖| بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = "📁| الملف ← "..file.."\n🔰| تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MEROTEAM/Files_MERO/master/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/TEAMMERO/Files_Boyka/master/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
